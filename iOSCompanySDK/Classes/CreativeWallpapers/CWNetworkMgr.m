470e59a62dedda5d3f93beb851dfb71f049682dd
GDayao <1418154979@qq.com>
2019-10-15 19:28:29 +0800

    update network manager file.

--- iOSCompanySDK/Classes/CreativeWallpapers/CWNetworkMgr.m
+++ iOSCompanySDK/Classes/CreativeWallpapers/CWNetworkMgr.m
@@ -11,7 +11,6 @@
                                   success:(void (^)(id _Nullable responseObject))success
                                   failure:(void (^)(NSError * _Nullable error))failure {
     AFHTTPSessionManager *sessionMgr = [AFHTTPSessionManager manager];
-    sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer];
     sessionMgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"application/xhtml+xml", @"application/xml", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", @"video/mp4", @"text/plain",@"charset=utf-8",nil];
     sessionMgr.requestSerializer.timeoutInterval = 60.f;
     [sessionMgr POST:URLString parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
--- iOSCompanySDK/Classes/NewWallpapersFull/NWFNetworkMgr.m
+++ iOSCompanySDK/Classes/NewWallpapersFull/NWFNetworkMgr.m
@@ -16,9 +16,7 @@
                                         parameters:(id)parameters
                                            success:(void (^)(id _Nullable responseObject))success
                                            failure:(void (^)(NSError * _Nullable error))failure {
-    //NWFCustomAFNetHTTPSessionMgr *sessionMgr = [NWFCustomAFNetHTTPSessionMgr manager];
     AFHTTPSessionManager *sessionMgr = [AFHTTPSessionManager manager];
-    //sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer];
     sessionMgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"application/xhtml+xml", @"application/xml", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", @"video/mp4", @"text/plain",@"charset=utf-8",nil];
     sessionMgr.requestSerializer.timeoutInterval = 60.f;
     [sessionMgr POST:URLString parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
