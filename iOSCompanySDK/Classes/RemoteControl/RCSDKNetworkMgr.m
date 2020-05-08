febdd9e6a24ab1ba46b45e787781280bd9f0a9d9
GDayao <1418154979@qq.com>
2020-05-08 09:55:26 +0800

    update get method handle responseObject

--- iOSCompanySDK/Classes/RemoteControl/RCSDKNetworkMgr.m
+++ iOSCompanySDK/Classes/RemoteControl/RCSDKNetworkMgr.m
@@ -35,7 +35,7 @@
 #pragma mark  - 'GET' net request data ----
 + (void)RCSDKAFHttpDataTaskGETMethodWithUrlString:(NSString *)UrlString parameters:(id)parameters success:(void (^)(id _Nullable responseObject))success failure:(void (^)(NSError * _Nullable error))failure {
     AFHTTPSessionManager *sessionMgr = [AFHTTPSessionManager manager];
-    sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //sessionMgr.responseSerializer = [AFHTTPResponseSerializer serializer]; // 此句加入不处理responseObject数据
     sessionMgr.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"application/xhtml+xml", @"application/xml", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", @"video/mp4", @"text/plain",@"charset=utf-8",nil];
     sessionMgr.requestSerializer.timeoutInterval = 6.f;
     [sessionMgr GET:UrlString parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
