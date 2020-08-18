73a8f39e8aac4c59949ea8c5ebc33bf4750e29c7
GDayao <1418154979@qq.com>
2020-08-18 10:43:26 +0800

    update VRSDKSystemMgr file

--- iOSCompanySDK/Classes/VoiceRecoder/VRSDKSystemMgr.h
+++ iOSCompanySDK/Classes/VoiceRecoder/VRSDKSystemMgr.h
@@ -19,9 +19,29 @@ NS_ASSUME_NONNULL_BEGIN
 @interface VRSDKSystemMgr : NSObject
 
 
+// get app config
++ (NSString *)getAppBundleId;
++ (NSString *)getAppDisplayName;
++ (NSString *)getAppVersion;
++ (NSString *)getAppBuildVersion;
+
+// 获取手机品牌
++ (NSString *)getDeviceBand;
+// 获取当前设备的操作系统版本号
++ (NSString *)getDeviceOSVersion;
+
+// 获取当前设备的UDID-存储到KeyChain中
++ (NSString *)getDeviceUDIDValueFromKeychain;
+
+// get current设备的idfa
++ (NSString *)getDeviceIDFA;
+
+// 获取网络类型
++ (NSString *)getDeviceNetworkStatus;
 
 
-
+// 获取设备型号
++ (NSString*)getDeviceType;
 
 
 @end
