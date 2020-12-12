8324af1f50bd6834fcfbf60733befbe8edd596e8
GDayao <1418154979@qq.com>
2020-12-12 20:29:34 +0800

    update all proj iphone type model.

--- iOSCompanySDK/Classes/CreativeWallpapers/CWSystemMgr.m
+++ iOSCompanySDK/Classes/CreativeWallpapers/CWSystemMgr.m
@@ -166,6 +166,18 @@
     if([platform isEqualToString:@"iPhone11,6"]) return@"iPhone XS Max";
     if([platform isEqualToString:@"iPhone11,8"]) return@"iPhone XR";
     
+    // iPhone 11 ++
+    if([platform isEqualToString:@"iPhone12,1"]) return @"iPhone 11";
+    if([platform isEqualToString:@"iPhone12,3"]) return @"iPhone 11 Pro";
+    if([platform isEqualToString:@"iPhone12,5"]) return @"iPhone 11 Pro Max";
+    if([platform isEqualToString:@"iPhone12,8"]) return @"iPhone SE(2ed generation)";
+    
+    // iPhone 12 ++
+    if([platform isEqualToString:@"iPhone13,1"]) return @"iPhone 12 mini";
+    if([platform isEqualToString:@"iPhone13,2"]) return @"iPhone 12";
+    if([platform isEqualToString:@"iPhone13,3"]) return @"iPhone 12  Pro";
+    if([platform isEqualToString:@"iPhone13,4"]) return @"iPhone 12  Pro Max";
+    
     if([platform isEqualToString:@"iPod1,1"]) return@"iPod Touch 1G";
     
     if([platform isEqualToString:@"iPod2,1"]) return@"iPod Touch 2G";
